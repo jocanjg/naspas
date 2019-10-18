104e170d293e4431722a45b17e628efaa661737c
jocanjg <jocan.jg@gmail.com>
2019-10-18 15:18:37 +0200

    naspas page

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -126,7 +126,7 @@
       </div>
       <div class="row">
           <div class="col-sm-5">
-            <div class="dataTables_info" id="example2_info" role="status" aria-live="polite">Prikaz {{count($animals)}} od {{count($animals)}}  </div>
+            <div class="dataTables_info" id="example2_info" role="status" aria-live="polite">Prikaz {{count($animals)}} od {{ $count }}  </div>
           </div>
           <div class="col-sm-7">
             <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
